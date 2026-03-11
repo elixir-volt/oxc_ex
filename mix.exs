e78573a426e219d7dbd8536ce3d92a564da27df6
Danila Poyarkov <dev@dannote.net>
2026-03-11 06:58:31 +0300

    Add ex_dna and ex_slop to lint pipeline

--- mix.exs
+++ mix.exs
@@ -24,6 +24,7 @@ defmodule OxcEx.MixProject do
       lint: [
         "format --check-formatted",
         "credo --strict",
+        "ex_dna",
         "dialyzer",
         "cmd cargo fmt --manifest-path native/oxc_ex_nif/Cargo.toml -- --check",
         "cmd cargo clippy --manifest-path native/oxc_ex_nif/Cargo.toml -- -D warnings"
@@ -36,7 +37,9 @@ defmodule OxcEx.MixProject do
       {:rustler, "~> 0.36.1"},
       {:rustler_precompiled, "~> 0.8"},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_dna, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:ex_slop, "~> 0.2", only: [:dev, :test], runtime: false}
     ]
   end
 end

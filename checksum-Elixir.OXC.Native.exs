3fee09548eb68b77416578d435d3098d567c3ee2
Danila Poyarkov <dev@dannote.net>
2026-03-17 15:35:57 +0300

    Update precompiled NIF checksums for v0.5.1

--- checksum-Elixir.OXC.Native.exs
+++ checksum-Elixir.OXC.Native.exs
@@ -1,7 +1,7 @@
 %{
-  "liboxc_ex_nif-v0.5.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:e506c45d4114e628990350de0996947690c91635dba0579b8232d709a13e82ee",
-  "liboxc_ex_nif-v0.5.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:b99a1e346527d009b0b0de50a76b00bf418e826124aaf5250be9e125ee3e7798",
-  "liboxc_ex_nif-v0.5.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:450a4d58f7e215c7bc22b6d3c74c033da52524befa39a1e043ba468d8b38cb7c",
-  "liboxc_ex_nif-v0.5.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:835107faa2c07a9a889af37e28c6472915738966e53b4a152cd417dac5c9c5da",
-  "liboxc_ex_nif-v0.5.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:76c40c830a06ba638c160de860cd66b5fbb3ef467ff9d9655f3cb58cd7de5936",
+  "liboxc_ex_nif-v0.5.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:2d258b052d0072e57b98937f9774e8d2e7f2695c1478a6c0bd900d98c06708c4",
+  "liboxc_ex_nif-v0.5.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:9062e0709ddf686a46bc81c1c547d2a939ddaadcbc49a0692fcc8479acec0586",
+  "liboxc_ex_nif-v0.5.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:9c42dff7b0f3c2acc9d530ae3debb333e1c1234efe9acd1d98ef6203d19f20be",
+  "liboxc_ex_nif-v0.5.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:b981bca9284e8cb6e46027ac0de25cf81da59834165eec6fb60329ed14692b33",
+  "liboxc_ex_nif-v0.5.1-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:05c67b9ad264b6f0fe1556c7561236306b98113bcc7c7fea5fd6eac0fa1e191d",
 }

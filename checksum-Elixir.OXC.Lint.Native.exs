4750db459aee21b95a807295c642fc626ac5ba06
Danila Poyarkov <dev@dannote.net>
2026-04-20 20:26:15 +0300

    Update precompiled NIF checksums for v0.8.0

--- checksum-Elixir.OXC.Lint.Native.exs
+++ checksum-Elixir.OXC.Lint.Native.exs
@@ -1,2 +1,6 @@
 %{
+  "liboxc_lint_nif-v0.8.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:0cb10fa8c1a9295909bb3d6daddc23ed463cb26b2aa4bd9899a0e9a0865d5dbe",
+  "liboxc_lint_nif-v0.8.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:0580de2fe1a2d2bc5a0a1b7d3e856f56d50b52147cdd9e2c21fa21eb614682d8",
+  "liboxc_lint_nif-v0.8.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:37f726d35aed3682972548ba463694cefba0e2551cf733ffc4ccac4da30f38ce",
+  "liboxc_lint_nif-v0.8.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:79a5653f027bce3fb84d835a09b165871afa9d54f24f46898c9ff72384b1e862",
 }
--- checksum-Elixir.OXC.Native.exs
+++ checksum-Elixir.OXC.Native.exs
@@ -1,7 +1,7 @@
 %{
-  "liboxc_ex_nif-v0.7.2-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:508eb61a9e3c8c0c2469f9b8e27176bdf9a32518212fac052741c13a45a2aa28",
-  "liboxc_ex_nif-v0.7.2-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:edd1461f69f7c922c8b14d5e0ca7b9d9cab8034f42d5c2adfbf708c8a08ebcf1",
-  "liboxc_ex_nif-v0.7.2-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:e045e03b7402e2ab4256128b3b874dae1211a12b53a807210acea448fbfff5d5",
-  "liboxc_ex_nif-v0.7.2-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:f4b23c4a7048da6b8a5cb420bf6112a6eced51138ff12043f96d496d08ee4926",
-  "liboxc_ex_nif-v0.7.2-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:a471f28070eea5dbb9814f255dd1f02e5ea8c397811a1d46515fdf0f1bbb264b",
+  "liboxc_ex_nif-v0.8.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:65d2befecdcfa2d3d4b8f6d6526c434777c6d122612f1ee191f7d818277c0a54",
+  "liboxc_ex_nif-v0.8.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:132ec85dcedf28eb484c392c1ddac091766ea8828b13ef8459eaa89a26ff64a7",
+  "liboxc_ex_nif-v0.8.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:68edde949676afef4c558f7549398e3945d60dc65eb7a58c93006c4433aeceb2",
+  "liboxc_ex_nif-v0.8.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:3198049c538d77e7ffb6ea65c6e2c0992e2743e1486ba4bc4172faf05892c578",
+  "liboxc_ex_nif-v0.8.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:fc01d10c8fd91ca2bfc98190207af770180060620f459f0d5edde7fcec1a332a",
 }

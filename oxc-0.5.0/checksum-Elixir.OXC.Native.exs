7c9061b57184c98e0668eba3475763c9affe7fbb
Danila Poyarkov <dev@dannote.net>
2026-07-16 21:11:24 +0200

    Remove accidentally committed package extraction

--- oxc-0.5.0/checksum-Elixir.OXC.Native.exs
+++
@@ -1,7 +0,0 @@
-%{
-  "liboxc_ex_nif-v0.5.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:e506c45d4114e628990350de0996947690c91635dba0579b8232d709a13e82ee",
-  "liboxc_ex_nif-v0.5.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:b99a1e346527d009b0b0de50a76b00bf418e826124aaf5250be9e125ee3e7798",
-  "liboxc_ex_nif-v0.5.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:450a4d58f7e215c7bc22b6d3c74c033da52524befa39a1e043ba468d8b38cb7c",
-  "liboxc_ex_nif-v0.5.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:835107faa2c07a9a889af37e28c6472915738966e53b4a152cd417dac5c9c5da",
-  "liboxc_ex_nif-v0.5.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:76c40c830a06ba638c160de860cd66b5fbb3ef467ff9d9655f3cb58cd7de5936",
-}
